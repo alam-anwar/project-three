be262b3f5f3ec4074d68cfd254f214caf5ccfa49
alam-anwar <alamanwar17@gmail.com>
2023-12-02 16:30:41 -0500

    added three sorts

--- main.cpp
+++ main.cpp
@@ -4,6 +4,136 @@
 #include <iostream>
 using namespace std;
 
+void merge(int *array, int l, int m, int r) {
+    int i, j, k, nl, nr;
+    nl = m-l+1; nr = r-m;
+    int larr[nl], rarr[nr];
+
+    for(i = 0; i<nl; i++)
+        larr[i] = array[l+i];
+
+    for(j = 0; j<nr; j++)
+        rarr[j] = array[m+1+j];
+
+    i = 0; j = 0; k = l;
+
+    while(i < nl && j<nr) {
+        if(larr[i] <= rarr[j]) {
+            array[k] = larr[i];
+            i++;
+        } else {
+            array[k] = rarr[j];
+            j++;
+        }
+        k++;
+    }
+    while(i<nl) {
+        array[k] = larr[i];
+        i++; k++;
+    }
+    while(j<nr) {
+        array[k] = rarr[j];
+        j++; k++;
+    }
+}
+
+void mergeSort(int *array, int l, int r) {
+    int m;
+    if(l < r) {
+        int m = l+(r-l)/2;
+
+        mergeSort(array, l, m);
+        mergeSort(array, m+1, r);
+        merge(array, l, m, r);
+    }
+}
+
+
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high- 1; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pivot = partition(arr, low, high);
+
+        quickSort(arr, low, pivot - 1);
+        quickSort(arr, pivot + 1, high);
+    }
+}
+
+int getMax(int arr[], int n)
+{
+    int mx = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > mx)
+            mx = arr[i];
+    return mx;
+}
+
+void countSort(int arr[], int n, int exp)
+{
+    int output[n];
+    int i, count[10] = { 0 };
+
+    for (i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+
+    for (i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+
+    for (i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+
+    for (i = 0; i < n; i++)
+        arr[i] = output[i];
+}
+
+void radixSort(int arr[], int n)
+{
+
+    int m = getMax(arr, n);
+
+    for (int exp = 1; m / exp > 0; exp *= 10)
+        countSort(arr, n, exp);
+}
+
 int main() {
-    cout << "project 1 sucked" << endl;
+    int arr[] = {369, 611, 965, 593, 81, 251, 840, 996, 303, 736};
+
+    for (int i = 0; i < 10; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    radixSort(arr, sizeof(arr) / sizeof(int));
+    cout << "After radix sort: ";
+
+    for (int i = 0; i < 10; i++) {
+        cout << arr[i] << " ";
+    }
 }
