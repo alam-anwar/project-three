83119cd0ecf74437843909d9b9665c12594b24d4
alam-anwar <alamanwar17@gmail.com>
2023-12-04 16:30:03 -0500

    100000 elements instead of 10000

--- main.cpp
+++ main.cpp
@@ -123,7 +123,7 @@ void radixSort(vector<int>& vec, int n)
 int main() {
     vector<int> vec;
 
-    for (int i = 0; i < 10000; i++){
+    for (int i = 0; i < 100000; i++){
         vec.push_back(rand());
     }
 
