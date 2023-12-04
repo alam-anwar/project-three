49223af1012e74264b700c88ffca5e636f724614
alam-anwar <alamanwar17@gmail.com>
2023-12-04 02:06:12 -0500

    changes to cmake???

---
+++ cmake-build-debug/CMakeFiles/project_three.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_three.dir/main.cpp.obj"
+  "CMakeFiles/project_three.dir/main.cpp.obj.d"
+  "libproject_three.dll.a"
+  "project_three.exe"
+  "project_three.exe.manifest"
+  "project_three.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_three.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
