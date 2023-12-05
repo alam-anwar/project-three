fd53681e943e61d60f56a754fc938bd0a05df4bf
John Aitken <johna321123@gmail.com>
2023-12-04 22:47:11 -0500

    final project

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,24 +1,67 @@
 #include "ofApp.h"
 
 using namespace std;
-
-// bool operator<(ofRectangle r1, ofRectangle r2) {
-//     return r1.getHeight() < r2.getHeight();
-// }
+using namespace std::chrono;
 
 //--------------------------------------------------------------
 void ofApp::setup(){
     gui.setup();
-    gui.add(N.setup("N", 5, 1, 100));
-    gui.add(button.setup("sort"));
+
+    gui.add(N.setup("N", 50, 1, 100));
+    gui.add(gen.setup("Randomly Generate"));
+    gui.add(qsButton.setup("Quick Sort"));
+    gui.add(msButton.setup("Merge Sort"));
+    gui.add(rsButton.setup("Radix Sort"));
+    gui.add(complexity.setup("complexity", ""));
+    gui.add(time.setup("time", ""));
+
     generateRectangles(N);
+
+    cooldown = 1000;
+    lastClick = 0;
 
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-    // rectHeights.clear();
-    // for (const auto& rect : rects) rectHeights.push_back(rect.getHeight());
+        if (qsButton){
+            auto start = high_resolution_clock::now();
+
+            quickSort(rects, 0, rects.size() - 1);
+
+            auto stop = high_resolution_clock::now();
+            auto dur = duration_cast<microseconds>(stop - start);
+
+            complexity.setup("complexity", "O(n*logn)");
+            time.setup("time", to_string(dur.count()) + " ms");
+        }
+        else if (rsButton) {
+            auto start = high_resolution_clock::now();
+
+            radixSort(rects, rects.size());
+
+            auto stop = high_resolution_clock::now();
+            auto dur = duration_cast<microseconds>(stop - start);
+
+            complexity.setup("complexity", "O(n*d)");
+            time.setup("time", to_string(dur.count()) + " ms");
+        }
+        else if (msButton) {
+            auto start = high_resolution_clock::now();
+
+            mergeSort(rects, 0, rects.size() - 1);
+
+            auto stop = high_resolution_clock::now();
+            auto dur = duration_cast<microseconds>(stop - start);
+
+            complexity.setup("complexity", "O(n*logn)");
+            time.setup("time", to_string(dur.count()) + " ms");
+        }
+        else if (gen) {
+            generateRectangles(N);
+            complexity.setup("complexity", "");
+            time.setup("time", "");
+        }
 }
 
 //--------------------------------------------------------------
@@ -40,16 +83,10 @@ void ofApp::generateRectangles(int N) {
     float y = ofGetHeight() - 10;
 
     for (int i = 0; i < N; i++) {
-        int height = (int)ofRandom(ofGetHeight() - 100);
+        int height = (int)ofRandom(ofGetHeight() - 200);
         ofRectangle rect(i*15, y, 10, -height);
         rects.push_back(rect);
     }
-}
-
-void ofApp::rect_swap(ofRectangle& r1, ofRectangle& r2) {
-    int r1x = r1.getX();
-    r1.setX(r2.getX());
-    r2.setX(r1x);
 }
 
 //--------------------------------------------------------------
@@ -63,7 +100,7 @@ void ofApp::keyPressed(int key){
     else if (key == 'm') {
         mergeSort(rects, 0, rects.size() - 1);
     }
-    else {
+    else if (key == 'g'){
         generateRectangles(N);
     }
 }
@@ -85,7 +122,6 @@ void ofApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-    //line.clear();
 }
 
 //--------------------------------------------------------------
@@ -150,6 +186,8 @@ void ofApp::merge(vector<ofRectangle>& vec, int left, int mid, int right) {
 }
 
 void ofApp::mergeSort(vector<ofRectangle>& vec, int left, int right) {
+    auto start = high_resolution_clock::now();
+
     if(left < right) {
         int mid = left + (right - left) / 2;
 
@@ -157,6 +195,9 @@ void ofApp::mergeSort(vector<ofRectangle>& vec, int left, int right) {
         mergeSort(vec, mid + 1, right);
         merge(vec, left, mid, right);
     }
+
+    auto stop = high_resolution_clock::now();
+    auto dur = duration_cast<microseconds>(stop - start);
 }
 
 
@@ -170,13 +211,11 @@ int ofApp::partition (vector<ofRectangle>& vec, int low, int high)
         if ((int)vec[j].getHeight() <= pivot)
         {
             i++;
-            // rect_swap(rects[i], rects[j]);
             int iH = vec[i].getHeight();
             vec[i].setHeight(vec[j].getHeight());
             vec[j].setHeight(iH);
         }
     }
-    // rect_swap(rects[i + 1], rects[high]);
     int iH = vec[i+1].getHeight();
     vec[i+1].setHeight(vec[high].getHeight());
     vec[high].setHeight(iH);
@@ -185,12 +224,17 @@ int ofApp::partition (vector<ofRectangle>& vec, int low, int high)
 
 void ofApp::quickSort(vector<ofRectangle>& vec, int low, int high)
 {
+    auto start = high_resolution_clock::now();
+
     if (low < high)
     {
         int pivot = partition(rects, low, high);
         quickSort(rects, low, pivot - 1);
         quickSort(rects, pivot + 1, high);
     }
+
+    auto stop = high_resolution_clock::now();
+    auto dur = duration_cast<microseconds>(stop - start);
 }
 
 int ofApp::getMax(vector<int> vec)
@@ -231,10 +275,15 @@ void ofApp::radixSort(vector<ofRectangle>& vec, int n)
         heights.push_back(-rect.getHeight());
     }
 
+    auto start = high_resolution_clock::now();
+
     int max = getMax(heights);
 
     for (int exp = 1; max / exp > 0; exp *= 10)
         countSort(heights, n, exp);
+
+    auto stop = high_resolution_clock::now();
+    auto dur = duration_cast<microseconds>(stop - start);
 
     rects.clear();
 
--- src/ofApp.h
+++ src/ofApp.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <algorithm>
 #include <iostream>
+#include <chrono>
 
 #include "ofMain.h"
 #include "ofxGui.h"
@@ -11,30 +12,45 @@
 class ofApp : public ofBaseApp{
 
 	public:
+        // gui elements
         ofxPanel gui;
         ofxIntSlider N;
-        ofxButton button;
+        ofxButton gen;
+        ofxButton qsButton;
+        ofxButton msButton;
+        ofxButton rsButton;
+        ofxLabel complexity;
+        ofxLabel time;
 
+        // main structure to store rectangles
         std::vector<ofRectangle> rects;
+        // container that keeps heights for convenience during radix sort
         std::vector<int> radixHeights;
-        
-		void setup();
-		void update();
-		void draw();
 
+        // cooldown time to prevent button from repeatedly executing on single click
+        int cooldown;
+        long long lastClick;
+
+        // function to generate N randomly sized rectangles 
         void generateRectangles(int N);
-        void rect_swap(ofRectangle& r1, ofRectangle& r2);
 
+        // quick sort and helper function
         void quickSort(vector<ofRectangle>& vec, int low, int high);
         int partition (vector<ofRectangle>& vec, int low, int high);
 
+        // merge sort and helper function
         void merge(vector<ofRectangle>& vec, int left, int mid, int right);
         void mergeSort(vector<ofRectangle>& vec, int left, int right);
 
-
+        // radix sort and helper functions
         int getMax(vector<int> vec);
         void countSort(vector<int>& vec, int n, int exp);
         void radixSort(vector<ofRectangle>& vec, int n);
+        
+        // openFrameworks native functions
+		void setup();
+		void update();
+		void draw();
 
 		void keyPressed(int key);
 		void keyReleased(int key);
